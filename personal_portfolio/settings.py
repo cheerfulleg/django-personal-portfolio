a63c354dbe6e8b94cd46e7746dbb9b56c1ab173c
cheerfulleg <61907072+cheerfulleg@users.noreply.github.com>
2020-09-22 23:33:01 +0300

    minor updates

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'portfolio.apps.PortfolioConfig',
     'crispy_forms',
-    'storages',
 ]
 
 MIDDLEWARE = [
@@ -136,12 +135,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-#
-#
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
